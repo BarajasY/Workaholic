7a61d2dc6fe84fc95c991b08c23d621b5bbab38f
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-04-08 11:33:04 -0600

    Almost done setting variables.

--- Client/src/components/AddPosting/AddPosting.tsx
+++ Client/src/components/AddPosting/AddPosting.tsx
@@ -3,21 +3,66 @@ import {AiOutlineDelete} from 'react-icons/ai'
 import './AddPosting.css';
 
 const AddPosting = () => {
-  const [val, setVal] = useState('')
+  const [Salary, setSalary] = useState('')
+  const [SalaryRate, setSalaryRate] = useState("/hr")
+  const [SalaryCurrency, setSalaryCurrency] = useState("")
   const [BenefitsArray, setBenefitsArray] = useState([""])
+  const [PostingTitle, setPostingTitle] = useState("")
+  const [PostingDescription, setPostingDescription] = useState("")
+  const [IsRemote, setIsRemote] = useState(false)
+  const [IsIndefinite, setIsIndefinite] = useState(false)
+  const [JobTypeArray, setJobTypeArray] = useState([""])
+  const [Location, setLocation] = useState("")
+  const [Duration, setDuration] = useState("")
   const [Benefit, setBenefit] = useState("")
-  const JobTypeArray = ["Full-Time", "Part-Time", "Freelance"]
+  const [PostingDate, setPostingDate] = useState("")
+  const JobType = ["Full-Time", "Part-Time", "Freelance"]
+
+  const EnlistBenefits = () => {
+    const benefitChecker = Benefit === "" || Benefit === " "
+    if(!benefitChecker) {
+      if(BenefitsArray[0] === "") {
+        setBenefitsArray([Benefit]);
+        setBenefit("");
+      } else {
+        setBenefitsArray([...BenefitsArray, Benefit])
+        setBenefit(""); 
+      }
+    }
+  }
+
+  const AddJobType = (type:string) => {
+    if(JobTypeArray[0] === "" || JobTypeArray[0] === null) {
+      setJobTypeArray([type])
+    } else {
+      const AlreadyIn = JobTypeArray.includes(type)
+      if(AlreadyIn) {
+        setJobTypeArray(JobTypeArray.filter(t => t!= type))
+      } else {
+        setJobTypeArray([...JobTypeArray, type])
+      }
+    }
+  }
+
+  const IsRemoteFunction = () => {
+    setIsRemote(!IsRemote)
+      setLocation("Remote")
+  }
+  const IsIndefiniteFunction = () => {
+    setIsIndefinite(!IsIndefinite)
+    setDuration("Indefinite")
+  }
 
   return (
     <div className="addPostingContainer">
         <div className="addPostingForm">
           <section>
             <h1>Título del puesto</h1>
-            <input type="text" />
+            <input type="text" onChange={(e) => setPostingTitle(e.target.value)}/>
           </section>
           <section>
             <h1>Descripción a detalle del puesto</h1>
-            <textarea name="" id="" cols={50} rows={20}></textarea>
+            <textarea name="" id="" cols={50} rows={20} onChange={(e) => setPostingDescription(e.target.value)}></textarea>
           </section>
           <section>
             <h1>Salario ofrecido</h1>
@@ -25,18 +70,18 @@ const AddPosting = () => {
               <input
                 type="number"
                 pattern="[0-9]*"
-                value={val}
+                value={Salary}
                 onChange={(e) =>
-                  setVal((v) => (e.target.validity.valid ? e.target.value : v))
+                  setSalary((v) => (e.target.validity.valid ? e.target.value : v))
                 }
               />
-              <select name="" id="">
+              <select name="SalaryRate" value={SalaryRate} onChange={(e) => setSalaryRate(e.target.value)}>
                 <option value="hr">/hr</option>
                 <option value="day">/day</option>
                 <option value="month">/month</option>
                 <option value="year">/year</option>
               </select>
-              <select name="" id="">
+              <select name="SalaryCurrency" value={SalaryCurrency} onChange={(e) => setSalaryCurrency(e.target.value)}>
                 <option value="MXN">$MXN</option>
                 <option value="USD">$USD</option>
                 <option value="CAD">$CAD</option>
@@ -47,8 +92,8 @@ const AddPosting = () => {
           <section>
             <h1>Tipo de puesto</h1>
             <article className='jobType'>
-              {JobTypeArray.map((item, index) => {
-                return <p key={index}>{item}</p>
+              {JobType.map((item, index) => {
+                return <p key={index} className={JobTypeArray.includes(item) ? "JobTypeSelected" : ""} onClick={() => AddJobType(item)}>{item}</p>
               })}
             </article>
           </section>
@@ -56,30 +101,42 @@ const AddPosting = () => {
             <article className="PostingAddress">
               <h1>Dirección completa</h1>
               <p>Remoto</p>
-              <input type="checkbox" className='AddressCheckbox'/>
+              <input type="checkbox" className='AddressCheckbox' onClick={() => IsRemoteFunction()}/>
             </article>
-            <input type="text" placeholder='Municipio, Estado, Colonia, Calle, Numero'/>
+            <input type="text" 
+            placeholder='Municipio, Estado, Colonia, Calle, Numero'
+            className={IsRemote ? "Unchangeable" : ""}
+            value={Location}
+            readOnly = {IsRemote}
+            onChange={(e) => setLocation(e.target.value)}/>
           </section>
           <section>
             <article className='PostingDuration'>
               <h1>Duración</h1>
               <p>Indefinido </p>
-              <input type="checkbox" />
+              <input type="checkbox" onClick={() => IsIndefiniteFunction()}/>
             </article>
             <article className='PostingDurationMonths'>
-              <input type="number"/>
+              <input type="number"
+              value={Duration}
+              className={IsIndefinite ? "Unchangeable" : ""}
+              readOnly={IsIndefinite}
+              onChange={(e) => setDuration(e.target.value)}/>
               <h1>Meses</h1>
             </article>
           </section>
           <section>
             <h1>Beneficios</h1>
             <article className="PostingBenefits">
-              <input type="text" onChange={(e) => setBenefit(e.target.value)}/>
-              <button onClick={() => BenefitsArray[0] === "" ? setBenefitsArray([Benefit]) : setBenefitsArray([...BenefitsArray, Benefit])}>Enlistar</button>
+              {/*On Press "Enter" EnlistBenefits function will trigger.
+                EnlistBenefits function adds Benefit to an array. */}
+              <input type="text" value={Benefit} onKeyDown={(e) => e.key === "Enter" ? EnlistBenefits() : null} onChange={(e) => setBenefit(e.target.value)}/>
+              <button onClick={() => EnlistBenefits()}>Enlistar</button>
             </article>
             <ul>
               {BenefitsArray.map((Benefit, i) => (
                 <li key={i}>{Benefit}<AiOutlineDelete className='PostingIcon'
+                //Uses .filter to delete the desired benefit from the list.
                 onClick={() => setBenefitsArray(BenefitsArray.filter(item => item !== Benefit))}/>
                 </li>
               ))}
--- src/main/java/com/yahir/Workaholic/Postings/Postings.java
+++ src/main/java/com/yahir/Workaholic/Postings/Postings.java
@@ -24,49 +24,29 @@ public class Postings {
         generator = "id_sequence_generator"
     )
     private Integer id;
-    private String title;
     private String businessName;
+    private String title;
     private String description;
     private String jobType;
-    private String tags;
     private Number salary;
     private String location;
     private String country;
+    private String duration;
+    private String date;
+    private String tags;
 
-    public String getCountry() {
-        return this.country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Postings)) {
-            return false;
-        }
-        Postings postings = (Postings) o;
-        return Objects.equals(id, postings.id) && Objects.equals(title, postings.title) && Objects.equals(businessName, postings.businessName) && Objects.equals(description, postings.description) && Objects.equals(jobType, postings.jobType) && Objects.equals(tags, postings.tags) && Objects.equals(salary, postings.salary) && Objects.equals(location, postings.location) && Objects.equals(country, postings.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, businessName, description, jobType, tags, salary, location, country);
-    }
-    
-    public Postings(Integer id, String title, String businessName, String description, String jobType, String tags, Number salary, String location, String country) {
+    public Postings(Integer id, String businessName, String title, String description, String jobType, Number salary, String location, String country, String duration, String date, String tags) {
         this.id = id;
-        this.title = title;
         this.businessName = businessName;
+        this.title = title;
         this.description = description;
         this.jobType = jobType;
-        this.tags = tags;
         this.salary = salary;
         this.location = location;
         this.country = country;
+        this.duration = duration;
+        this.date = date;
+        this.tags = tags;
     }
 
     public Integer getId() {
@@ -77,20 +57,20 @@ public class Postings {
         this.id = id;
     }
 
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getBusinessName() {
         return this.businessName;
     }
 
     public void setBusinessName(String businessName) {
         this.businessName = businessName;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
@@ -109,14 +89,6 @@ public class Postings {
         this.jobType = jobType;
     }
 
-    public String getTags() {
-        return this.tags;
-    }
-
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
-
     public Number getSalary() {
         return this.salary;
     }
@@ -131,6 +103,126 @@ public class Postings {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDuration() {
+        return this.duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTags() {
+        return this.tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public Postings id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public Postings businessName(String businessName) {
+        setBusinessName(businessName);
+        return this;
+    }
+
+    public Postings title(String title) {
+        setTitle(title);
+        return this;
+    }
+
+    public Postings description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+    public Postings jobType(String jobType) {
+        setJobType(jobType);
+        return this;
+    }
+
+    public Postings salary(Number salary) {
+        setSalary(salary);
+        return this;
+    }
+
+    public Postings location(String location) {
+        setLocation(location);
+        return this;
+    }
+
+    public Postings country(String country) {
+        setCountry(country);
+        return this;
+    }
+
+    public Postings duration(String duration) {
+        setDuration(duration);
+        return this;
+    }
+
+    public Postings date(String date) {
+        setDate(date);
+        return this;
+    }
+
+    public Postings tags(String tags) {
+        setTags(tags);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Postings)) {
+            return false;
+        }
+        Postings postings = (Postings) o;
+        return Objects.equals(id, postings.id) && Objects.equals(businessName, postings.businessName) && Objects.equals(title, postings.title) && Objects.equals(description, postings.description) && Objects.equals(jobType, postings.jobType) && Objects.equals(salary, postings.salary) && Objects.equals(location, postings.location) && Objects.equals(country, postings.country) && Objects.equals(duration, postings.duration) && Objects.equals(date, postings.date) && Objects.equals(tags, postings.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, businessName, title, description, jobType, salary, location, country, duration, date, tags);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", businessName='" + getBusinessName() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", jobType='" + getJobType() + "'" +
+            ", salary='" + getSalary() + "'" +
+            ", location='" + getLocation() + "'" +
+            ", country='" + getCountry() + "'" +
+            ", duration='" + getDuration() + "'" +
+            ", date='" + getDate() + "'" +
+            ", tags='" + getTags() + "'" +
+            "}";
     }
 
 }
