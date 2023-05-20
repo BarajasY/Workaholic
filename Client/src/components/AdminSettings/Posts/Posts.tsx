c032bf045f187291dfeebcd66c89aa7da913ccf0
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-05-20 16:30:30 -0600

    Adding admin features

--- Client/src/components/AdminSettings/Posts/Posts.tsx
+++ Client/src/components/AdminSettings/Posts/Posts.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import './Posts.css'
+import React from "react";
+import "./Posts.css";
+import { useQuery } from "@tanstack/react-query";
+import { PostingInterface } from "../../../types";
+import { AiFillDelete } from "react-icons/ai";
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
-}
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["queryAllPosts"],
+    queryFn: async () => {
+      const get = await fetch("http://localhost:8080/api/v1/postings/all");
+      return await get.json();
+    },
+  });
 
-export default Posts
+  return (
+    <div className="postsContainer">
+      <h1>List of Posts</h1>
+      <div className="postsTableColumns">
+        <h1>Title</h1>
+        <h1>Salary</h1>
+        <h1>Currency</h1>
+        <h1>Rate</h1>
+        <h1>Duration</h1>
+        <h1>Date</h1>
+        <h1>Company</h1>
+        <h1>Country</h1>
+      </div>
+      {data.map((post: PostingInterface) => (
+        <div className="postWrapper">
+          <div className="postRecord">
+            <h1>{post.title}</h1>
+            <h1>{post.salary}</h1>
+            <h1>{post.currency.code}</h1>
+            <h1>{post.rate.rateName}</h1>
+            <h1>{post.duration === 0 ? "Indefinite" : post.duration}</h1>
+            <h1>{post.date}</h1>
+            <h1>{post.user.name}</h1>
+            <h1>{post.user.country.name}</h1>
+          </div>
+          <AiFillDelete className="deleteIcon"/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
--- Client/src/components/AdminSettings/Users/Users.tsx
+++ Client/src/components/AdminSettings/Users/Users.tsx
@@ -20,9 +20,22 @@ const Users = () => {
   return (
     <div className="usersContainer">
       <h1>List of Users</h1>
+      <div className="usersTableColumns">
+        <h1>Id</h1>
+        <h1>Name</h1>
+        <h1>Email</h1>
+        <h1>Country</h1>
+        <h1>Role</h1>
+      </div>
       <div className="usersTable">
         {data.map((user:userStateInterface) => (
-          <h1>{user.name}</h1>
+          <div className="userRecord">
+            <h1>{user.id.toString()}</h1>
+            <h1>{user.name}</h1>
+            <h1>{user.email}</h1>
+            <h1>{user.country.name}</h1>
+            <h1>{user.role.name}</h1>
+          </div>
           ))}
       </div>
     </div>
