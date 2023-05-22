80f7e236bfe96a23f23b2c590af39794d1c5424a
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-05-22 13:57:53 -0600

    improving ui in home

--- Client/src/components/Home/Home.tsx
+++ Client/src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import './Home.css'
+import { AiFillFilePdf } from 'react-icons/ai'
 
 const Home = () => {
   return (
     <div className="homeContainer">
+      <div className="homeContent1">
       <p>Presenting</p>
         <h1>
             <span>W</span>
@@ -22,6 +24,22 @@ const Home = () => {
             <h1>Your place to look out for </h1>
             <Link to="/signup">Jobs</Link>
         </div>
+      </div>
+      <div className="homeContent2">
+        <section>
+          <h1><span>2</span> Simple Steps</h1>
+        </section>
+        <section>
+          <h1>Prepare your <span>Resume</span></h1>
+          <AiFillFilePdf className='homeIcon'/>
+        </section>
+        <section>
+          <h1>And create an <span>account</span> with us!</h1>
+        </section>
+        <section>
+          <h1>Then you're ready to begin your <span>job hunt</span>.</h1>
+        </section>
+      </div>
     </div>
   )
 }
